2fb31b47a4137d9a553c2e5c00b2b8c1362c5ddd
dabuchera <bucher@ibi.baug.ethz.ch>
2023-12-19 19:24:05 +0100

    vercel ready

--- components/dashboard-nav.tsx
+++ components/dashboard-nav.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-import { SidebarNavItem } from "types"
-import { cn } from "@/lib/utils"
-import { Icons } from "@/components/icons"
+import { Icons } from '@/components/icons';
+import { cn } from '@/lib/utils';
+import { SidebarNavItem } from '@/types';
 
 interface DashboardNavProps {
   items: SidebarNavItem[]
