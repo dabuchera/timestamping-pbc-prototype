71a8ab676806bde5a25f893a15244245802503d9
dabuchera <bucher@ibi.baug.ethz.ch>
2024-01-15 02:19:20 +0100

    vercel

--- auth.config.ts
+++ auth.config.ts
@@ -13,6 +13,8 @@ export const authConfig = {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
+        console.log("nextUrl")
+        console.log(nextUrl)
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
